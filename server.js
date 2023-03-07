5a6347a0a92610c105f7313a4cd3c053b5700d6f
jhonnyeth <jasvofficial@gmail.com>
2023-03-07 16:00:27 -0500

    Part 2

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const app = express();
 const { Restaurant } = require("./models/index");
 const { sequelize } = require("./db");
 
-const port = 3000;
+const port = 3500;
 
 //TODO: Create your GET Request Route Below:
 app.get("/restaurants", async (request, response) => {
   response.json(await Restaurant.findAll());
+});
+
+//Part 2
+app.get("/restaurants/:id", async (request, response) => {
+  const restaurant = await Restaurant.findByPk(request.params.id);
+  response.json(restaurant);
 });
 
 app.listen(port, () => {
